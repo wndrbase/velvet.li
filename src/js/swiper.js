bb85ef01d1ddc071502b9580eb750b9f9e43b82e
htmlpluscss <79198889134@ya.ru>
2020-09-17 18:07:28 +0300

    swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -13,67 +13,22 @@
 	Array.from(swiperContainer, swipe => {
 
 		let mySwipe = null,
-			toggleSwipe = null,
-			resetSwipe = null;
+			toggleSwipe = null;
 
-		const swipeControls = document.createElement('div'),
-			  swipeNav = document.createElement('div'),
-			  swipeBtns = document.createElement('div'),
-			  swipeNext = document.createElement('button'),
-			  swipePrev = document.createElement('button'),
-			  items = swipe.querySelectorAll('.swiper-slide'),
-			  count = items.length,
-			  card = swipe.classList.contains('swiper-container--card'),
-			  product = swipe.classList.contains('swiper-container--product'),
-			  billboard = swipe.classList.contains('swiper-container--billboard');;
+		const swipeNav = document.createElement('div'),
+			  processStep = swipe.classList.contains('swiper-container--process');
 
 		swipeNav.className = 'swiper-pagination';
-		swipeControls.className = 'swiper-controls';
 
-		swipeBtns.className = 'swiper-navigation';
-		swipePrev.className = 'swiper-button-prev button';
-		swipeNext.className = 'swiper-button-next button';
-
-		swipePrev.innerHTML = '<svg width="8" height="14" viewBox="0 0 8 14"><path d="M.16 7.38l6.48 6.46a.54.54 0 10.77-.77L1.31 7 7.4.93a.54.54 0 00-.77-.77L.16 6.62a.54.54 0 000 .77z"/></svg>';
-		swipeNext.innerHTML = '<svg width="8" height="14" viewBox="0 0 8 14"><path d="M7.4 6.62L.93.16a.54.54 0 10-.77.77L6.25 7 .16 13.07a.54.54 0 00.77.77L7.4 7.38a.54.54 0 000-.77z"/></svg>';
-
-		swipeBtns.appendChild(swipePrev);
-		swipeBtns.appendChild(swipeNext);
-		swipeControls.appendChild(swipeBtns);
-		swipeControls.appendChild(swipeNav);
-		swipe.parentNode.appendChild(swipeControls);
+		swipe.appendChild(swipeNav);
 
 		// eager
 		Array.from(swipe.querySelectorAll('[loading="lazy"]'), img => img.setAttribute('loading','eager'));
 
-		// hide
-		Array.from(items, el => el.classList.remove('hide'));
+		if (processStep) {
 
-		resetSwipe = () => {
-
-			if(mySwipe) {
-
-				mySwipe.destroy(false,true);
-				mySwipe = undefined;
-
-			}
-
-			swipeNav.classList.add('hide');
-			swipeControls.classList.add('hide');
-
-		}
-
-		resetSwipe();
-
-		if (card) {
-
-			if(swipe.classList.contains('swiper-container--navigation')){
-
-				swipeBtns.classList.add('hide');
-				swipeNav.classList.remove('hide');
-				swipeControls.classList.remove('hide');
-
-			}
+			const current = swipe.querySelector('.process__swiper-current'),
+				  total = swipe.querySelectorAll('.swiper-slide').length;
 
 			toggleSwipe = () => {
 
@@ -81,64 +36,17 @@
 
 				new Swiper(swipe, {
 					loop: true,
-					slidesPerView: 'auto',
+					autoHeight: true,
+					preloadImages: false,
 					pagination: {
 						el: swipeNav,
 						clickable: true,
 						bulletElement: 'button',
 						bulletClass: 'button',
 						bulletActiveClass: 'is-active'
-					}
-				});
-
-			}
-
-		}
-
-		if (product) {
-
-			swipeControls.classList.remove('hide');
-			swipePrev.classList.add('swiper-button-disabled');
-
-			toggleSwipe = () => {
-
-				toggleSwipe = false;
-
-				new Swiper(swipe, {
-					loop: false,
-					preloadImages: false,
-					navigation: {
-						nextEl: swipeNext,
-						prevEl: swipePrev
-					}
-				});
-
-			}
-
-		}
-
-		if (billboard) {
-
-			swipeBtns.classList.add('hide');
-			swipeNav.classList.remove('hide');
-			swipeControls.classList.remove('hide');
-
-			toggleSwipe = () => {
-
-				toggleSwipe = false;
-
-				new Swiper(swipe, {
-					loop: true,
-					preloadImages: false,
-					autoplay: {
-						delay: 5000
 					},
-					pagination: {
-						el: swipeNav,
-						clickable: true,
-						bulletElement: 'button',
-						bulletClass: 'button',
-						bulletActiveClass: 'is-active'
+					on: {
+						slideChange: () => current.textContent = (swipe.swiper.realIndex % total + 1)
 					}
 				});
 
@@ -146,26 +54,7 @@
 
 		}
 
-		PubSub.subscribe('windowWidthResize', () => {
-
-			if (window.Swiper && toggleSwipe) {
-
-				toggleSwipe();
-
-			}
-
-		});
-
-		if(window.Swiper && toggleSwipe) {
-
-			toggleSwipe();
-
-		}
-		else {
-
-			PubSub.subscribe('swiperJsLoad', toggleSwipe);
-
-		}
+		PubSub.subscribe('swiperJsLoad', toggleSwipe);
 
 		if(!swiperInit) {
 
@@ -179,7 +68,7 @@
 
 			script.onload = () => PubSub.publish('swiperJsLoad');
 
-			setTimeout( () => document.head.appendChild(script), 7000);
+			setTimeout( () => document.head.appendChild(script), window.pageYOffset === 0 ? 7000 : 100);
 
 		}
 
