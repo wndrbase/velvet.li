0d10bb433d66ab85072f1100da53269edeaca6d2
htmlpluscss <79198889134@ya.ru>
2020-09-18 16:31:00 +0300

    бегущии строки

---
+++ src/js/percents.js
@@ -0,0 +1,41 @@
+((rows) => {
+
+	if(rows.length) {
+
+		Array.from(rows, row => {
+
+			const inner = row.querySelector('.percents__row-inner');
+
+			row.append(inner.cloneNode(true));
+
+		});
+
+		if ('IntersectionObserver' in window) {
+
+			const box = document.querySelector('.percents');
+
+			const options = {
+				root: null,
+				rootMargin: '0px',
+				threshold: [0.01]
+			};
+
+			const callback = (entries, observer) => {
+
+				Array.prototype.forEach.call(entries, (entry) => {
+
+					box.classList.toggle('in-viewport', entry.isIntersecting);
+
+				});
+
+			};
+
+			const observer = new IntersectionObserver(callback, options);
+
+			observer.observe(box);
+
+		}
+
+	}
+
+})(document.querySelectorAll('.percents__row'));
