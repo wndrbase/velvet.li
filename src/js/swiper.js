ef98adfc4eebfee72f3a683ef8cdde30863c812f
htmlpluscss <79198889134@ya.ru>
2020-09-18 19:58:12 +0300

    process

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -13,7 +13,8 @@
 	Array.from(swiperContainer, swipe => {
 
 		let mySwipe = null,
-			toggleSwipe = null;
+			toggleSwipe = null,
+			resetSwipe = null;
 
 		const swipeNav = document.createElement('div'),
 			  processStep = swipe.classList.contains('swiper-container--process');
@@ -25,34 +26,171 @@
 		// eager
 		Array.from(swipe.querySelectorAll('[loading="lazy"]'), img => img.setAttribute('loading','eager'));
 
-		if (processStep) {
+		resetSwipe = () => {
 
-			const current = swipe.querySelector('.process__swiper-current'),
-				  total = swipe.querySelectorAll('.swiper-slide').length;
+			if(mySwipe) {
 
-			toggleSwipe = () => {
-
-				toggleSwipe = false;
-
-				new Swiper(swipe, {
-					loop: true,
-					autoHeight: true,
-					preloadImages: false,
-					pagination: {
-						el: swipeNav,
-						clickable: true,
-						bulletElement: 'button',
-						bulletClass: 'button',
-						bulletActiveClass: 'is-active'
-					},
-					on: {
-						slideChange: () => current.textContent = (swipe.swiper.realIndex % total + 1)
-					}
-				});
+				mySwipe.destroy(false,true);
+				mySwipe = undefined;
 
 			}
 
 		}
+
+		if (processStep) {
+
+			let inViewport = true;
+
+			if ('IntersectionObserver' in window) {
+
+				const options = {
+					root: null,
+					rootMargin: '0px',
+					threshold: [0.3]
+				};
+
+				const callback = (entries, observer) => {
+
+					Array.prototype.forEach.call(entries, (entry) => {
+
+						inViewport = entry.isIntersecting;
+
+					});
+
+				};
+
+				const observer = new IntersectionObserver(callback, options);
+
+				observer.observe(document.querySelector('.process__right'));
+
+			}
+
+			const current = swipe.querySelector('.process__swiper-current'),
+				  total = swipe.querySelectorAll('.swiper-slide').length,
+				  processBtn = document.querySelectorAll('.process__btn'),
+				  circle = document.querySelector('.process__btn.is-active circle'),
+				  pi2r = parseInt(circle.getAttribute('r')) * 2 * Math.PI;
+
+			let idTimer = null;
+
+			const Circle = (btn, circle) => {
+
+				btn.classList.add('is-active');
+
+				let count = 0;
+
+				idTimer = setInterval( () => {
+
+					if(count === 100) {
+
+						swipe.swiper.slideNext();
+
+					}
+
+					if(inViewport) {
+
+						count++;
+
+					}
+
+					circle.setAttribute('stroke-dasharray', pi2r / 100 * count + ' ' + pi2r);
+
+				}, 50);
+
+			}
+
+			toggleSwipe = () => {
+
+				resetSwipe();
+
+				if(window.innerWidth < 1200) {
+
+					mySwipe = new Swiper(swipe, {
+						loop: true,
+						autoHeight: true,
+						preloadImages: false,
+						pagination: {
+							el: swipeNav,
+							clickable: true,
+							bulletElement: 'button',
+							bulletClass: 'button',
+							bulletActiveClass: 'is-active'
+						},
+						on: {
+							slideChangeTransitionEnd: () => {
+								current.textContent = (swipe.swiper.realIndex % total + 1);
+							}
+						}
+					});
+
+				} else {
+
+					mySwipe = new Swiper(swipe, {
+						preloadImages: false,
+						direction: 'vertical',
+						loop: true,
+						on: {
+							init: () => {
+
+								Array.from(processBtn, (btn,index) =>
+									btn.addEventListener('click', () =>
+										swipe.swiper.slideToLoop(index)));
+
+								Circle(processBtn[0],circle);
+
+							},
+							slideChangeTransitionEnd: () => {
+
+								clearInterval(idTimer);
+
+								Array.from(processBtn, (btn,index) => {
+
+									const old = index < swipe.swiper.realIndex,
+										  circle = btn.querySelector('circle');
+
+									if(index === swipe.swiper.realIndex % total) {
+
+										Circle(btn,circle);
+
+									}
+									else {
+
+										if(old) {
+
+											btn.classList.add('is-active');
+											circle.setAttribute('stroke-dasharray', '0 0');
+
+										}
+										else {
+
+											btn.classList.remove('is-active');
+
+										}
+
+									}
+
+								});
+
+							}
+						}
+					});
+
+
+				}
+
+			}
+
+		}
+
+		PubSub.subscribe('windowWidthResize', () => {
+
+			if (window.Swiper) {
+
+				toggleSwipe();
+
+			}
+
+		});
 
 		PubSub.subscribe('swiperJsLoad', toggleSwipe);
 
@@ -68,7 +206,7 @@
 
 			script.onload = () => PubSub.publish('swiperJsLoad');
 
-			setTimeout( () => document.head.appendChild(script), window.pageYOffset === 0 ? 7000 : 100);
+			setTimeout( () => document.head.appendChild(script), window.pageYOffset === 0 ? 70 : 100);
 
 		}
 
