8818255b441c9aba942487f4e55cbb3f453ac32d
htmlpluscss <79198889134@ya.ru>
2020-10-07 10:08:16 +0300

    cookie

---
+++ src/js/cookie.js
@@ -0,0 +1,30 @@
+((cookie) => {
+
+	"use strict";
+
+	if(cookie) {
+
+		if(Cookies.get('YourPrivacy') !== 'Accept') {
+
+			cookie.classList.remove('hide');
+
+			const AnalyticsCookies = cookie.querySelector('[name="AnalyticsCookies"]'),
+				  AdvertsitingCookies = cookie.querySelector('[name="AdvertsitingCookies"]');
+
+			cookie.addEventListener('submit', event => {
+
+				event.preventDefault();
+
+				Cookies.set('YourPrivacy', 'Accept');
+				Cookies.set('AnalyticsCookies', AnalyticsCookies.checked);
+				Cookies.set('AdvertsitingCookies', AdvertsitingCookies.checked);
+
+				cookie.classList.add('hide');
+
+			});
+
+		}
+
+	}
+
+})(document.querySelector('.cookie'));
