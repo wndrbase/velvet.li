01255015316240289a696d56d5e3d2ea974322d5
htmlpluscss <79198889134@ya.ru>
2021-03-26 16:29:33 +0300

    form, blog, chat

--- src/js/hbspt.js
+++ src/js/hbspt.js
@@ -9,7 +9,8 @@
 	const hbsptFormsCreate = form => {
 
 		hbspt.forms.create({
-			portalId: "8473950",
+			region: "na1",
+			portalId: "19575538",
 			formId: form.getAttribute('data-hbspt-form'),
 			target: '#' + form.getAttribute('id')
 		});
